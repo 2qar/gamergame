1f8939f216295d352f603805025391ec36b10fdb
BigHeadGeorge <tucker1014@hotmail.com>
2018-01-29 23:09:34 -0800

    Pretty bullet trails, pretty text

---
+++ Assets/Scripts/General/ParentTextFinder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParentTextFinder : MonoBehaviour
+{
+    GameObject parentTextObj;
+    Text parentText;
+    Text objText;
+	// Use this for initialization
+	void Start ()
+    {
+        parentTextObj = GameObject.FindGameObjectWithTag(gameObject.name.Substring(0, name.Length - 2));
+        parentText = parentTextObj.GetComponent<Text>();
+        objText = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        objText.text = parentText.text;
+        objText.fontSize = parentText.fontSize;
+	}
+}
